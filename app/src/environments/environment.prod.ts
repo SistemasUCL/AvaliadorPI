9ff46838ef6a69d0841304050b5dc088283e0987
Táriton Neves <tariton.neves@itixti.com.br>
2019-06-14 19:33:51 -0300

    alter

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { IEnvironment } from './model';
 export const environment: IEnvironment = {
   production: true,
   VERSION: require('../../package.json').version,
-  API_URL: 'https://API_HOST/api',
-  AUTH_URL: 'https://IDENTITY_HOST',
+  API_URL: 'https://avaliadorpi-api.azurewebsites.net/api',
+  AUTH_URL: 'https://avaliadorpi-identity.azurewebsites.net'
 };
