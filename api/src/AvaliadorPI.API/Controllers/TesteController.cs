6c09e97c6e9c1392cb88a8aab6581c61c8cffad4
Alielson Piffer <alielson.piffer@aevo.com.br>
2019-11-20 17:30:47 -0300

    Controles para Teste

---
+++ api/src/AvaliadorPI.API/Controllers/TesteController.cs
@@ -0,0 +1,50 @@
+﻿using AvaliadorPI.Domain.RootProjeto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvaliadorPI.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Teste")]
+    public class TesteController : Controller
+    {
+        private readonly IProjetoService _projetoService;
+
+        public TesteController(IProjetoService projetoService)
+        {
+            _projetoService = projetoService;
+        }
+
+        // GET: api/Teste
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var x = _projetoService.ObterDadosGrid(null).Result;
+            return Ok(x);
+        }
+
+        // GET: api/Teste/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Teste
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Teste/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ api/src/AvaliadorPI.Identity/Controllers/TesteController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AvaliadorPI.Identity.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Teste")]
+    public class TesteController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TesteController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: api/Teste
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _userManager.Users.Select(x => x.UserName);
+
+            return new string[] { "value1", "value2", };
+        }
+
+        // GET: api/Teste/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Teste
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Teste/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
